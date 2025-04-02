fbd9bd71313a5195dc6d46770e6b4222d5fdaff3
mdorn <matt.dorn@datadoghq.com>
2025-04-02 12:59:31 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+import time
+import random
+
+app = Flask(__name__)
+
+@app.route('/time')
+def get_current_time():
+    timestamp = str(int(time.time()))
+    response = {
+        "data": {"unix_timestamp": timestamp},
+        "message": "success"
+    }
+    return response
+
+
+@app.route('/random')
+def get_random_numbers():
+    # v = random.random()
+    numbers = [random.random() for _ in range(10)]
+    response = {
+        "data": {"random_number": numbers},
+        "message": "success"
+    }
+    return response
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
