68db772cac011e40e20247b765da3900434ae8df
mdorn <matt.dorn@datadoghq.com>
2025-04-03 21:53:07 -0500

    Add Terraform config

---
+++ infra/terraform/ecs-sg.tf
@@ -0,0 +1,37 @@
+resource "aws_security_group" "ecs_sg" {
+  name        = "${var.prefix}-ecs-sg"
+  description = "Security group for ECS cluster"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "Allow HTTP traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow port 5000"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow port 8000"
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
