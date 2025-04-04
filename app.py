49caa1a1a7d6b0986ff6c3919b1b65adb74a4440
mdorn <matt.dorn@datadoghq.com>
2025-04-03 21:51:44 -0500

    Add IaC etc.

--- app.py
+++ app.py
@@ -1,37 +1,52 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import time
 import random
+import sqlite3
 
-VERSION=2
-
+VERSION=1
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "App version {}".format(VERSION)
+    return jsonify({
+        'app_version': VERSION,
+        'endpoints': [
+            '/user?id=',
+            '/random'
+        ]
+    })
 
-@app.route('/time')
-def get_current_time():
-    timestamp = str(int(time.time()))
-    response = {
-        "data": {"unix_timestamp": timestamp},
-        "message": "success"
-    }
-    return response
+
+@app.route('/user')
+def get_records():
+    '''Function intentionally vulnerable to SQL injection'''
+    uid = request.args.get('id')
+    conn = sqlite3.connect(':memory:')
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)')
+    cursor.execute("INSERT INTO users (username, password) VALUES ('foo', 'P@ssw0rd#')")
+    cursor.execute("INSERT INTO users (username, password) VALUES ('bar', 'S3cr3t!')")
+    # SQLi example: http://localhost:5000/users?id=1%20OR%201=1
+    cursor.execute("SELECT * FROM users WHERE id={}".format(uid))  # VULNERABLE
+    # cursor.execute("SELECT * FROM users WHERE id= ?", [uid])  NOT VULNERABLE: parameterized query
+    result = jsonify(cursor.fetchall())
+    conn.close()
+    return result
 
 
 @app.route('/random')
 def get_random_numbers():
+    '''Function with intentional misuse of random.randrange function'''
     id = 1
     # sql = f"SELECT id,description FROM notes WHERE id = {id}"
-    # numbers = [random.random() for _ in range(10)]
-    numbers = [random.randint(0, 5) for _ in range(10)]
+    numbers = [random.randrange(10) for _ in range(10)]  # VULNERABLE
+    # numbers = [random.randint(0, 5) for _ in range(10)]  # NOT VULNERABLE - better yet, use secrets module
     response = {
         "data": {"random_number": numbers},
         "message": "success"
     }
-    return response
+    return jsonify(response)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='127.0.0.1', port=5000)
